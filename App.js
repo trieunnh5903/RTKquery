242d44ebf57bc9a3cf7fbf9078198ba08b0912fd
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-08-10 14:20:31 +0700

    delete

--- App.js
+++ App.js
@@ -1,87 +1,124 @@
-import { ActivityIndicator, Button, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native'
-import React, { useRef, useState } from 'react'
-import { Provider, useDispatch } from "react-redux";
-import { useAddNewSubjectMutation, useGetAllSubjectsQuery, } from './src/redux/api';
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-undef */
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import {
+  useAddNewSubjectMutation,
+  useDeleteSubjectMutation,
+  useGetAllSubjectsQuery,
+} from './src/redux/api';
 import { store } from './src/redux/store';
-import { fetchPostRequest } from './src/redux/slice';
 import { setUpServer } from './src/redux/fakeApi/mock_api';
 
 if (window.server) {
-  server.shutdown()
+  server.shutdown();
 }
 
 window.server = setUpServer();
-
 
 const App = () => {
   return (
     <Provider store={store}>
       <AppComponent />
     </Provider>
-  )
-}
+  );
+};
 
 const AppComponent = () => {
-  const {
-    data,
-    isFetching,
-    isSuccess,
-    isError,
-    error
-  } = useGetAllSubjectsQuery();
+  const { data, isFetching, isSuccess, isError, error } =
+    useGetAllSubjectsQuery();
   const [id, setId] = useState(null);
   const [name, setName] = useState('');
-  const [addNewSubject, { isLoading }] = useAddNewSubjectMutation()
+  const [addNewSubject] = useAddNewSubjectMutation();
+  const [deleteSubject] = useDeleteSubjectMutation();
+
+  // handle delete subject mutation
+  const onDeletePress = (subjectId) => {
+    deleteSubject(subjectId);
+  };
+
   // render the subject
   let content;
   if (isFetching) {
     content = <ActivityIndicator size={'small'} />;
   } else if (isSuccess) {
-    content = data.subjects.map(subject =>
-      <View key={subject.id} style={{ padding: 8, flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ flex: 1, color: 'black', padding: 8 }} >{subject.name}</Text>
-        <TouchableOpacity style={{ backgroundColor: 'red' }}>
-          <Text style={{ color: 'white', padding: 8 }} >DELETE</Text>
+    content = data.subjects.map(subject => (
+      <View
+        key={subject.id}
+        style={{ padding: 8, flexDirection: 'row', alignItems: 'center' }}>
+        <Text style={{ flex: 1, color: 'black', padding: 8 }}>
+          {subject.name}
+        </Text>
+        <TouchableOpacity onPress={() => onDeletePress(subject.id)} style={{ backgroundColor: 'red' }}>
+          <Text style={{ color: 'white', padding: 8 }}>DELETE</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{ backgroundColor: 'cyan' }} >
-          <Text style={{ padding: 8 }} >EDIT</Text>
+        <TouchableOpacity style={{ backgroundColor: 'cyan' }}>
+          <Text style={{ padding: 8 }}>EDIT</Text>
         </TouchableOpacity>
       </View>
-    )
+    ));
   } else if (isError) {
-    content = <Text>{error.toString()}</Text>
+    content = <Text>{error.toString()}</Text>;
   }
 
   //handle click add button
   const onSubmtPress = async () => {
     if (id && name) {
       try {
-        await addNewSubject({ id, name }).unwrap()
+        await addNewSubject({ id, name }).unwrap();
         setId('');
         setName('');
-        console.log("addNewSubject success")
-      } catch (error) {
-        console.log("addNewSubject failure: " + error)
+        console.log('addNewSubject success');
+      } catch (e) {
+        console.log(e.message);
       }
-    }else{
-      console.log("emty")
+    } else {
+      console.log('emty');
     }
-  }
+  };
   return (
     <View style={{ padding: 24 }}>
       <Text>ID:</Text>
-      <TextInput value={id} onChangeText={setId} style={{ borderWidth: 1, borderRadius: 10 }} />
+      <TextInput
+        value={id}
+        onChangeText={setId}
+        style={{ borderWidth: 1, borderRadius: 10 }}
+      />
       <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} style={{ borderWidth: 1, borderRadius: 10 }} />
-      <TouchableOpacity onPress={onSubmtPress} style={{ height: 50, marginTop: 10, width: '100%', backgroundColor: "violet", justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ color: "white" }}>Submit</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        style={{ borderWidth: 1, borderRadius: 10 }}
+      />
+      <TouchableOpacity onPress={onSubmtPress} style={styles.btnSubmit}>
+        <Text style={{ color: 'white' }}>Submit</Text>
       </TouchableOpacity>
-      <Text style={{ color: 'black', fontSize: 20, marginTop: 20 }}>All subjects</Text>
+      <Text style={{ color: 'black', fontSize: 20, marginTop: 20 }}>
+        All subjects
+      </Text>
       {content}
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  btnSubmit: {
+    height: 50,
+    marginTop: 10,
+    width: '100%',
+    backgroundColor: 'violet',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/redux/api.js
+++ src/redux/api.js
@@ -1,56 +1,56 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const subjectApi = createApi({
-    reducerPath: 'subjectApi',
-    baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-    tagTypes: ["Subject"],
-    endpoints: (builder) => ({
-        getAllSubjects: builder.query({
-            query: () => '/subjects',
-            providesTags: ['Subject']
-        }),
+  reducerPath: 'subjectApi',
+  baseQuery: fetchBaseQuery({baseUrl: '/api'}),
+  tagTypes: ['Subject'],
+  endpoints: builder => ({
+    getAllSubjects: builder.query({
+      query: () => '/subjects',
+      providesTags: ['Subject'],
+    }),
 
-        getSubjectById: builder.query({
-            query: (id) => `/subjects/${id}`
-        }),
+    getSubjectById: builder.query({
+      query: id => `/subjects/${id}`,
+    }),
 
-        deleteSubject: builder.mutation({
-            query: (id) => ({
-                url: `/subjects/${id}`,
-                method: 'DELETE',
-                credentials: 'include',
-                // Include the entire post object as the body of the request
-            }),
-            invalidatesTags: ['Subject']
-        }),
+    deleteSubject: builder.mutation({
+      query: id => ({
+        url: `/subjects/${id}`,
+        method: 'DELETE',
+        credentials: 'include',
+        // Include the entire post object as the body of the request
+      }),
+      invalidatesTags: ['Subject'],
+    }),
 
-        addNewSubject: builder.mutation({
-            query: initialPost => ({
-                url: '/subjects',
-                method: 'POST',
-                // Include the entire post object as the body of the request
-                body: initialPost,
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                }
-            }),
-            invalidatesTags: ['Subject']
-        }),
+    addNewSubject: builder.mutation({
+      query: initialPost => ({
+        url: '/subjects',
+        method: 'POST',
+        // Include the entire post object as the body of the request
+        body: initialPost,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      }),
+      invalidatesTags: ['Subject'],
+    }),
 
-        // editSubject: builder.mutation({
-        //     query: subject => ({
-        //         url: `/subject/${subject.id}`,
-        //         method: 'PUT',
-        //         body: subject
-        //     })
-        // })
-    })
-})
+    // editSubject: builder.mutation({
+    //     query: subject => ({
+    //         url: `/subject/${subject.id}`,
+    //         method: 'PUT',
+    //         body: subject
+    //     })
+    // })
+  }),
+});
 
-export const { 
-    // useEditSubjectMutation,
-    useGetAllSubjectsQuery,
-    useGetSubjectByIdQuery,
-    useDeleteSubjectMutation,
-    useAddNewSubjectMutation } = subjectApi
+export const {
+  // useEditSubjectMutation,
+  useGetAllSubjectsQuery,
+  useGetSubjectByIdQuery,
+  useDeleteSubjectMutation,
+  useAddNewSubjectMutation,
+} = subjectApi;
